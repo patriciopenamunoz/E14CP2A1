3c5f3c324816f5de58ea7d7d6c908b54872ab92f
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-09-12 00:24:11 -0300

    Add resources routes for employees

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :areas
-  resources :companies
+  resources :companies do
+    resources :employees, only: [:create]
+  end
   root 'companies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
