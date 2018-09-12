982dbca3b3b238f89f11061de6186dd1f84e4230
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-09-12 00:34:20 -0300

    Add create method for employees controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,8 @@
 class EmployeesController < ApplicationController
+  def create
+    Employee.create(employee_params)
+  end
+
   def employee_params
     params.require('employee').permit(%I[first_name last_name email])
   end
