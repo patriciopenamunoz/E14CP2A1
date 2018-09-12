1ecbb084c767199db21c4393568d10fe43b84e6a
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-09-12 20:10:51 -0300

    Stylize site

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -2,13 +2,13 @@ class EmployeesController < ApplicationController
   def create
     @company = Company.find(params['company_id'])
     @employee = @company.employees.create(employee_params)
-    redirect_to @company
+    redirect_to @company, notice: 'Employee was successfully created.'
   end
 
   def destroy
     @company = Company.find(params[:company_id])
     Employee.delete(params[:id])
-    redirect_to @company
+    redirect_to @company, notice: 'Employee was successfully removed.'
   end
 
   def employee_params
